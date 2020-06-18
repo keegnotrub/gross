7c4734bac556a1d8433ce81f358a532a07cd4b24
keegnotrub <keegnotrub@icloud.com>
2020-06-18 12:59:23 -0500

    WIP

--- config/initializers/hypernova.rb
+++ config/initializers/hypernova.rb
@@ -1,8 +1,4 @@
-require 'hypernova/plugins/development_mode_plugin'
-
 Hypernova.configure do |config|
-  config.host = "localhost"
+  config.host = "0.0.0.0"
   config.port = "3030"
 end
-
-Hypernova.add_plugin!(DevelopmentModePlugin.new)
